a8677b2886d52e5ad28ba88e1da9d9e0efe162cd
Rebecca Strauss <rstrauss127@gmail.com>
2018-11-07 18:09:07 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,13 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+        erb :'super_hero'
+    end
+
+    post '/teams' do
+      @params = params[:team]
+      erb :team
+    end
 
 end
